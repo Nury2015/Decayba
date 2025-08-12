1fb5bb1777bcd522460144b272be33613c10f987
Nury2015 <decayba@gmail.com>
2025-08-12 16:50:52 -0500

    se inyecta menumobile

--- src/assets/js/modules/mobile.js
+++ src/assets/js/modules/mobile.js
@@ -1,12 +1,14 @@
-// mobile.js
 export function activarMenuHamburguesa() {
   const hamburger = document.getElementById("hamburger");
   const mobileMenu = document.querySelector(".mobile-menu");
 
-  if (hamburger && mobileMenu) {
-    hamburger.addEventListener("click", () => {
-      hamburger.classList.toggle("active");
-      mobileMenu.classList.toggle("active");
-    });
+  if (!hamburger || !mobileMenu) {
+    console.error('No se encontró el menú hamburguesa');
+    return;
   }
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+  });
 }
