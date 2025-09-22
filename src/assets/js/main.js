e0fec450072024296c32a6370a4d6c619fd5c13e
Nury2015 <decayba@gmail.com>
2025-09-22 13:48:51 -0500

    prueba 3

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,6 @@
 import { renderCart } from "/Decayba/src/assets/js/modules/renderCart.js";
 import { actualizarCarritoVisual } from "/Decayba/src/assets/js/modules/carrito.js";
-import { verDetalleProducto } from "/Decayba/src/js/assets/modules/detalleProducto.js";
+import { verDetalleProducto } from "/Decayba/src/assets/js/modules/detalleProducto.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
