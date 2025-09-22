868436539e874aa965ab395822a26ede59c34264
Nury2015 <decayba@gmail.com>
2025-09-22 12:28:36 -0500

    ajuste nombre atributo

--- src/assets/js/modules/carrito.js
+++ src/assets/js/modules/carrito.js
@@ -53,7 +53,7 @@ function renderCart2() {
 
   carrito.forEach((item, index) => {
     //const rutaFinal = item.imagen.replace("./src", "/Decayba/src");    
-    const rutaFinal = item.imagen;   
+    const rutaFinal = item.img;   
     console.log(rutaFinal)
     const div = document.createElement("div");
     div.classList.add("shopping-cart");
